044581a158c27d6969290f231f26caeccd92abd2
musta <mustafa774519161@gmail.com>
2024-08-16 14:56:10 +0300

    add path catgory

--- v1/include/tables/categories/attribute.php
+++ v1/include/tables/categories/attribute.php
@@ -10,4 +10,9 @@ class CategoriesAttribute
   public $order = "order";
   public $createdAt = "createdAt";
   public $updatedAt = "updatedAt";
+  // 
+  function path_image()
+  {
+    return __DIR__ . "/../images/categories/";
+  }
 }
