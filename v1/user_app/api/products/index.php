72314f4ffd163cf9a83c95581eb20be5f257650a
musta <mustafa774519161@gmail.com>
2024-08-16 16:56:49 +0300

    qwe

--- v1/user_app/api/products/index.php
+++ v1/user_app/api/products/index.php
@@ -1,7 +1,7 @@
 <?php
 namespace UserApp;
 
-require_once "../../app/products/index.php";
+require_once __DIR__ . "/../../app/products/index.php";
 
 
 class ThisClass
@@ -19,18 +19,8 @@ class ThisClass
 
     function main(): string
     {
-        if (getTag()  == "read") {
+        if (getTag() == "read") {
             return $this->read();
-        } elseif (getTag()  == "add") {
-            return $this->add();
-        }elseif (getTag()  == "search") {
-            return $this->search();
-        } elseif (getTag()  == "updateName") {
-            return $this->updateName();
-        } elseif (getTag()  == "updateSha") {
-            return $this->updateSha();
-        } elseif (getTag()  == "updateVersion") {
-            return $this->updateVersion();
         } else {
             UNKOWN_TAG();
         }
@@ -41,35 +31,6 @@ class ThisClass
         $resultData = $this->controller->read();
         return json_encode($resultData);
 
-    }
-    private function add(): string
-    {
-        $resultData = $this->controller->add();
-        return json_encode($resultData);
-
-    }
-
-    private function search(): string
-    {
-
-        $resultData = $this->controller->search($this->getSearch());
-        return json_encode($resultData);
-    }
-    // 
-    private function updateName(): string
-    {
-        $resultData = $this->controller->updateName($this->getInputAppId(), $this->getInputAppPackageName());
-        return json_encode($resultData);
-    }
-    private function updateSha(): string
-    {
-        $resultData = $this->controller->updateSha($this->getInputAppId(), $this->getInputAppSha());
-        return json_encode($resultData);
-    }
-    private function updateVersion(): string
-    {
-        $resultData = $this->controller->updateVersion($this->getInputAppId(), $this->getInputAppVersion());
-        return json_encode($resultData);
     }
 }
 
--- v1/user_app/app/products/index.php
+++ v1/user_app/app/products/index.php
@@ -10,13 +10,6 @@ require_once 'executer.php';
 
 class Products
 {
-    // private $check;
-
-    // public function __construct()
-    // {
-    //     $this->check = new CheckPermission();
-    // }
-
     function read()
     {
         // checkPermission("READ_GROUPS");
@@ -24,44 +17,6 @@ class Products
         // getProjectLoginTokenData("RUN_APP", $s);
         return getProductsExecuter()->executeGetData();
     }
-    function add()
-    {
-        // checkPermission("READ_GROUPS");
-        $s = getMainRunApp();
-        getProjectLoginTokenData("RUN_APP", $s);
-        return getProductsExecuter()->executeAddData(getInputCategoryId(), getInputProductName(), getInputProductNumber(), getInputProductPostPrice(), getInputProductImage(), getInputProductGroupId());
-    }
-
-
-    function updateName($id, $newValue)
-    {
-        $resultData = $this->check->check("UPDATE_GROUP_NAME");
-        checkProjectIdSU($resultData);
-        return getAppsExecuter()->executeUpdateName($resultData, $id, $newValue);
-    }
-
-    function updateSha($id, $newValue)
-    {
-        $resultData = $this->check->check("UPDATE_GROUP_NAME");
-        checkProjectIdSU($resultData);
-        return getAppsExecuter()->executeUpdateSha($resultData, $id, $newValue);
-    }
-    function updateVersion($id, $newValue)
-    {
-        $resultData = $this->check->check("UPDATE_GROUP_NAME");
-        checkProjectIdSU($resultData);
-        return getAppsExecuter()->executeUpdateVersion($resultData, $id, $newValue);
-    }
-
-    function search($search)
-    {
-        $resultData = $this->check->check("ADD_GROUP");
-        checkProjectIdSU($resultData);
-        return getAppsExecuter()->executeSearchData($search);
-    }
-
-
-
 }
 
 
