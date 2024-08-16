4f837ac6b5e774b2289b15e972685c4c37f2cfee
musta <mustafa774519161@gmail.com>
2024-08-16 15:14:43 +0300

    fix public key

--- v1/get_public_key.php
+++ v1/get_public_key.php
@@ -1,13 +1,13 @@
 <?php
-require_once ("middleware.php");
-sleep("5");
-$publickey = fread(fopen($GLOBALS["path"] . "keys/publicKey.pem", "r"), 10000);
+
+
+$publickey = fread(fopen(__DIR__ . "/include/keys/publicKey.pem", "r"), 10000);
 
 // $key = shared_execute_read1_no_json_sql("SELECT server_key_public FROM server_keys WHERE server_key_id = '1'")[0];
 // sleep(5);
 // die(preg_replace('/^.+\n/','',$publickey));
-$publickey = str_replace('-----BEGIN PUBLIC KEY-----','',$publickey);
-$publickey = str_replace('-----END PUBLIC KEY-----','',$publickey);
+$publickey = str_replace('-----BEGIN PUBLIC KEY-----', '', $publickey);
+$publickey = str_replace('-----END PUBLIC KEY-----', '', $publickey);
 $publickey = trim($publickey);
 
 
