14e70d7acac8b99de5eef0d16e70d984a28c8135
musta <mustafa774519161@gmail.com>
2024-08-19 18:03:57 +0300

    fef

--- v1/include/projects/sql.php
+++ v1/include/projects/sql.php
@@ -6,7 +6,7 @@ class ProjectsSql extends \ProjectsAttribute
     function readByIdSql($id): string
     {
         $table_name = $this->table_name;
-        $columns = "$this->id,$this->serviceAccountKey";
+        $columns = "$this->id,$this->serviceAccountKey,$this->latLong";
         $innerJoin = "";
         $condition = "$this->id = $id FOR UPDATE";
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
