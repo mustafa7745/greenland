bdd655bc8cd93329524acf3d4c599484e374f666
musta <mustafa774519161@gmail.com>
2024-08-20 18:33:26 +0300

    dsd

--- v1/delivery_app/app/reservations/executer.php
+++ v1/delivery_app/app/reservations/executer.php
@@ -24,9 +24,9 @@ class ReservationsExecuter
       require_once (getDeliveryPath() . 'app/orders/helper.php');
       $orderDelivery = getOrdersDeliveryHelper()->getDataById(getOrderDeliveryId($acceptance));
       $order = getOrdersHelper()->getDataById($orderDelivery[getOrdersDeliveryHelper()->orderId]);
-      if ($order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_COMPLETED || $order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_CENCELED) {
-        return ["success" => "false"];
-      }
+      // if ($order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_COMPLETED || $order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_CENCELED) {
+      //   return ["success" => "false"];
+      // }
       $ordersProducts = getOrdersProductsHelper()->getOrderProductsByOrderWithItsStuff1(getId($order));
       $ordersProducts["acceptStatus"] = getAcceptanceHelper()->WAIT_TO_ACCEPT_STATUS;
       $ordersProducts["systemOrderNumber"] = $order[getOrdersHelper()->systemOrderNumber];
@@ -45,9 +45,9 @@ class ReservationsExecuter
 
       $orderDelivery = getOrdersDeliveryHelper()->getDataById(getOrderDeliveryId($acceptance));
       $order = getOrdersHelper()->getDataById($orderDelivery[getOrdersDeliveryHelper()->orderId]);
-      if ($order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_COMPLETED || $order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_CENCELED) {
-        return ["success" => "false"];
-      }
+      // if ($order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_COMPLETED || $order[getOrdersHelper()->situationId] == getOrdersHelper()->ORDER_CENCELED) {
+      //   return ["success" => "false"];
+      // }
       // print_r($order);
       $situatinId = $order[getOrdersHelper()->situationId];
       if ($situatinId != getOrdersHelper()->ORDER_COMPLETED and $situatinId != getOrdersHelper()->ORDER_CENCELED) {
