705602241ed21e8b4e61d1905cd58723351164d5
musta <mustafa774519161@gmail.com>
2024-08-21 07:07:27 +0300

    sdf

--- v1/include/tables/orders_discounts/post_data.php
+++ v1/include/tables/orders_discounts/post_data.php
@@ -1,32 +1,32 @@
 <?php 
-function getInputOrderId()
-{
-    $name = "inputOrderId";
-    if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
-        EMPTY_OR_NOT_FOUND($name);
-    }
-    $value = getPostData3()[$name];
-    $value = mysqli_real_escape_string(getDB()->conn, $value);
-    return $value;
-}
-function getInputOrderSystemNumber()
-{
-    $name = "inputOrderSystemNumber";
-    if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
-        EMPTY_OR_NOT_FOUND($name);
-    }
-    $value = getPostData3()[$name];
-    $value = mysqli_real_escape_string(getDB()->conn, $value);
-    return $value;
-}
-function getInputOrderDeliveryId()
-{
-    $name = "inputOrderDeliveryId";
-    if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
-        EMPTY_OR_NOT_FOUND($name);
-    }
-    $value = getPostData3()[$name];
-    // checkIfNumber($value);
-    $value = mysqli_real_escape_string(getDB()->conn, $value);
-    return $value;
-}
+// function getInputOrderId()
+// {
+//     $name = "inputOrderId";
+//     if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
+//         EMPTY_OR_NOT_FOUND($name);
+//     }
+//     $value = getPostData3()[$name];
+//     $value = mysqli_real_escape_string(getDB()->conn, $value);
+//     return $value;
+// }
+// function getInputOrderSystemNumber()
+// {
+//     $name = "inputOrderSystemNumber";
+//     if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
+//         EMPTY_OR_NOT_FOUND($name);
+//     }
+//     $value = getPostData3()[$name];
+//     $value = mysqli_real_escape_string(getDB()->conn, $value);
+//     return $value;
+// }
+// function getInputOrderDeliveryId()
+// {
+//     $name = "inputOrderDeliveryId";
+//     if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
+//         EMPTY_OR_NOT_FOUND($name);
+//     }
+//     $value = getPostData3()[$name];
+//     // checkIfNumber($value);
+//     $value = mysqli_real_escape_string(getDB()->conn, $value);
+//     return $value;
+// }
