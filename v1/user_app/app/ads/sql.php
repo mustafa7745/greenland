dd4f9bbce6ad8c5928d0686c0ad7423a2d1ea341
musta <mustafa774519161@gmail.com>
2024-08-22 16:27:09 +0300

    vdv

--- v1/user_app/app/ads/sql.php
+++ v1/user_app/app/ads/sql.php
@@ -5,13 +5,14 @@ require_once (getPath() . 'tables/ads/attribute.php');
 
 class AdsSql extends \AdsAttribute
 {
-    
+
     function readSql(): string
     {
+        $date = getCurruntDate();
         $table_name = $this->table_name;
         $columns = getColumnImagePath(" * ", "ads_image_path");
         $innerJoin = "";
-        $condition = "1";
+        $condition = "DATE($this->createdAt) =  DATE('$date')";
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
     }
 }
