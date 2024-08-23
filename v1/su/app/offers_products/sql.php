22cebf049692e755451e430e51222b13b05850ae
musta <mustafa774519161@gmail.com>
2024-08-23 21:37:31 +0300

    dsed

--- v1/su/app/offers_products/sql.php
+++ v1/su/app/offers_products/sql.php
@@ -8,8 +8,8 @@ class OffersProductsSql extends \OffersProductsAttribute
     function readByIdSql($id): string
     {
         $table_name = $this->table_name;
-        $columns = "*";
-        $innerJoin = "";
+        $innerJoin = $this->INNER_JOIN();
+        $columns = "$this->table_name.$this->id,$this->table_name.$this->productQuantity ,{$this->products_attribute->table_name}.{$this->products_attribute->name},{$this->products_attribute->table_name}.{$this->products_attribute->postPrice}";
         $condition = "$this->id = $id";
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
     }
