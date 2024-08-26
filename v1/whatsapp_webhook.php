0e6f8b547d500868e905ba3577243bffa1ab8cfb
musta <mustafa774519161@gmail.com>
2024-08-26 20:58:37 +0300

    verify code

--- v1/whatsapp_webhook.php
+++ v1/whatsapp_webhook.php
@@ -1,2 +1,6 @@
-<?php 
-echo "mustafa";
+<?php
+$hub_verify_token = "774519161";
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token']) && $_GET["hub_verify_token"] === $hub_verify_token) {
+   echo $_GET['hub_challenge'];
+   exit;
+}
