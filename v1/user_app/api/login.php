60e484e0d1ef1bfb2439f29b762aef30f3bfb360
musta <mustafa774519161@gmail.com>
2024-08-27 22:19:13 +0300

    vfdf

--- v1/user_app/api/login.php
+++ v1/user_app/api/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../include/check/index.php';
+require_once __DIR__ . '/../../include/login/index.php';
 
 require_once __DIR__ . '/../../include/check/login_tokens/helper.php';
 
