a4357fb37a198d93aa40470a6e005ae1e01fd390
musta <mustafa774519161@gmail.com>
2024-08-27 21:51:11 +0300

    gdg

--- v1/user_app/api/refresh_token.php
+++ v1/user_app/api/refresh_token.php
@@ -12,21 +12,10 @@ class ThisClass
     // $login = login();
     $runApp = getMainRunApp();
     // print_r($runApp);
-    $loginToken = refreshUserLoginToken($runApp, 1);
+    $userLoginToken = refreshUserLoginToken($runApp, 1);
     // print_r($loginToken);
-    $data2 = json_encode(
-      array("token" => getLoginToken($loginToken), "expire_at" => getExpireAt($loginToken))
-    );
-    $encryptedData = encrypt(
-      $data2,
-      getPublicKeyFormat(
-        getPublicKey(
-          getDevice(
-            $runApp
-          )
-        )
-      )
-    );
+    $data2 = json_encode(array("token" => $userLoginToken->loginToken, "expire_at" => $userLoginToken->expireAt));
+    $encryptedData = encrypt($data2, getPublicKeyFormat($runApp->device->publicKey));
     // sleep(5);
     shared_execute_sql("COMMIT");
     return json_encode(
