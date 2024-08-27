94daf1abdeda895d91bda0688abfd03575a2606e
musta <mustafa774519161@gmail.com>
2024-08-27 17:37:49 +0300

    fdff

--- v1/whatsapp_webhook.php
+++ v1/whatsapp_webhook.php
@@ -35,7 +35,7 @@ if (isset($input)) {
                     $m = $m . "مرحبا بك";
                     $m = $m . "\n";
                     $m = "الرقم السري هو: " . $password;
-                    $w->sendMessageText($phone_number, "dfgg");
+                    $w->sendMessageText($phone_number, $w);
                 }
             }
         }
