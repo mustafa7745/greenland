a79751b3e374b0f6e8c89e8d9025281010e04bb6
musta <mustafa774519161@gmail.com>
2024-08-27 16:41:46 +0300

    hfhtgh

---
+++ v1/_user.php
@@ -0,0 +1,70 @@
+<?php 
+require_once __DIR__ . '/../v1/include/tables/users/attribute.php' ;
+
+class UsersSql extends \UsersAttribute
+{
+    function addSql($deliveryManId): string
+    {
+        $date = getCurruntDate();
+        $table_name = $this->table_name;
+        $columns = "(`$this->id`,`$this->deliveryManId`,`$this->createdAt`,`$this->updatedAt`)";
+        $values = "(NULL,$deliveryManId,'$date','$date')";
+        /////
+        return shared_insert_sql($table_name, $columns, $values);
+    }
+    function readsql($phone): string
+    {
+        $table_name = $this->table_name;
+        $columns = " * ";
+        $innerJoin = "";
+        $condition = "$this->phone = $phone";
+        /////
+        return shared_read_sql($table_name, $columns, $innerJoin, $condition);
+    }
+    function readByIdsql($id): string
+    {
+        $table_name = $this->table_name;
+        $columns = " * ";
+        $innerJoin = "";
+        $condition = "$this->id = $id FOR UPDATE";
+        /////
+        return shared_read_sql($table_name, $columns, $innerJoin, $condition);
+    }
+}
+
+class UsersHelper extends UsersSql
+{
+  function getData($phone)
+  {
+    $sql = $this->readsql("'$phone'");
+    $data = shared_execute_read1_no_json_sql($sql);
+    if (count($data) != 1) {
+      return null;
+    }
+    return $data[0];
+  }
+  function getDataById($id)
+  {
+    $sql = $this->readByIdSql($id);
+
+    $data = shared_execute_read1_no_json_sql($sql);
+
+    if (count($data) != 1) {
+      $ar = $this->name . "_ID_ERROR";
+      $en = $this->name . "_ID_ERROR";
+      exitFromScript($ar, $en);
+    }
+    return $data[0];
+  }
+}
+
+$users_helper = null;
+function getUsersHelper()
+{
+  global $users_helper;
+  if ($users_helper == null) {
+    $users_helper = (new UsersHelper());
+  }
+  return $users_helper;
+}   
+
--- v1/whatsapp_webhook.php
+++ v1/whatsapp_webhook.php
@@ -18,6 +18,7 @@ if (isset($input)) {
             $name = $input['entry'][0]['changes'][0]['value']['contacts'][0]['profile']['name'];
             $w->sendMessageText($phone_number,"1");
 
+            require_once __DIR__ .'/../v1/_user.php';
             $user = getUsersHelper()->getData($phone);
             if ($user == null) {
                 $w->sendMessageText($phone_number,"no user");
@@ -28,73 +29,5 @@ if (isset($input)) {
         }
     }
 }
-require_once __DIR__ . '/../v1/include/tables/users/attribute.php' ;
-
-class UsersSql extends \UsersAttribute
-{
-    function addSql($deliveryManId): string
-    {
-        $date = getCurruntDate();
-        $table_name = $this->table_name;
-        $columns = "(`$this->id`,`$this->deliveryManId`,`$this->createdAt`,`$this->updatedAt`)";
-        $values = "(NULL,$deliveryManId,'$date','$date')";
-        /////
-        return shared_insert_sql($table_name, $columns, $values);
-    }
-    function readsql($phone): string
-    {
-        $table_name = $this->table_name;
-        $columns = " * ";
-        $innerJoin = "";
-        $condition = "$this->phone = $phone";
-        /////
-        return shared_read_sql($table_name, $columns, $innerJoin, $condition);
-    }
-    function readByIdsql($id): string
-    {
-        $table_name = $this->table_name;
-        $columns = " * ";
-        $innerJoin = "";
-        $condition = "$this->id = $id FOR UPDATE";
-        /////
-        return shared_read_sql($table_name, $columns, $innerJoin, $condition);
-    }
-}
-
-class UsersHelper extends UsersSql
-{
-  function getData($phone)
-  {
-    $sql = $this->readsql("'$phone'");
-    $data = shared_execute_read1_no_json_sql($sql);
-    if (count($data) != 1) {
-      return null;
-    }
-    return $data[0];
-  }
-  function getDataById($id)
-  {
-    $sql = $this->readByIdSql($id);
-
-    $data = shared_execute_read1_no_json_sql($sql);
-
-    if (count($data) != 1) {
-      $ar = $this->name . "_ID_ERROR";
-      $en = $this->name . "_ID_ERROR";
-      exitFromScript($ar, $en);
-    }
-    return $data[0];
-  }
-}
-
-$users_helper = null;
-function getUsersHelper()
-{
-  global $users_helper;
-  if ($users_helper == null) {
-    $users_helper = (new UsersHelper());
-  }
-  return $users_helper;
-}   
 
 
