5c186d149b556e6a4144de6847fea5728de58d94
musta <mustafa774519161@gmail.com>
2024-08-31 16:09:17 +0300

    fix bug delivery app

--- v1/delivery_app/api/acceptance/index.php
+++ v1/delivery_app/api/acceptance/index.php
@@ -19,28 +19,20 @@ class ThisClass
 
     function main(): string
     {
-        if (getTag()  == "read") {
-            return $this->read();
-        }elseif (getTag()  == "accept") {
+        if (getTag() == "reject") {
+            return $this->reject();
+        } elseif (getTag() == "accept") {
             return $this->accept();
-        } elseif (getTag()  == "add") {
+        } elseif (getTag() == "add") {
             return $this->add();
-        }elseif (getTag()  == "search") {
-            return $this->search();
-        } elseif (getTag()  == "updateName") {
-            return $this->updateName();
-        } elseif (getTag()  == "updateSha") {
-            return $this->updateSha();
-        } elseif (getTag()  == "updateVersion") {
-            return $this->updateVersion();
         } else {
             UNKOWN_TAG();
         }
     }
     //Main Functin CRUD
-    private function read(): string
+    private function reject(): string
     {
-        $resultData = $this->controller->read();
+        $resultData = $this->controller->reject();
         return json_encode($resultData);
 
     }
@@ -55,29 +47,6 @@ class ThisClass
         $resultData = $this->controller->accept();
         return json_encode($resultData);
 
-    }
-
-    private function search(): string
-    {
-
-        $resultData = $this->controller->search($this->getSearch());
-        return json_encode($resultData);
-    }
-    // 
-    private function updateName(): string
-    {
-        $resultData = $this->controller->updateName($this->getInputAppId(), $this->getInputAppPackageName());
-        return json_encode($resultData);
-    }
-    private function updateSha(): string
-    {
-        $resultData = $this->controller->updateSha($this->getInputAppId(), $this->getInputAppSha());
-        return json_encode($resultData);
-    }
-    private function updateVersion(): string
-    {
-        $resultData = $this->controller->updateVersion($this->getInputAppId(), $this->getInputAppVersion());
-        return json_encode($resultData);
     }
 }
 
