317e189ea8cad2fffb0c8b320f7dee3fd111abbc
musta <mustafa774519161@gmail.com>
2024-09-03 13:37:14 +0300

    grgh

--- v1/project_manager/app/collections/executer.php
+++ v1/project_manager/app/collections/executer.php
@@ -9,8 +9,8 @@ class CollectionsExecuter
     require_once __DIR__ . '/../../app/orders/executer.php';
     $orderExecuter = getOrdersExecuter();
     $collections = getCollectionsHelper()->getData($deliveryManId);
-    $sum = 0;
     for ($i = 0; $i < count($collections); $i++) {
+      $sum = 0;
       $orderId = $collections[$i][getCollectionsHelper()->orderId];
       require_once __DIR__ . '/../../../include/shared_app/order-content/index.php';
       $data = (new \OrderContent());
