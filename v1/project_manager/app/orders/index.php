8376a2f96eb0be71155ba7b7dae2e2c8c1849d4e
musta <mustafa774519161@gmail.com>
2024-09-03 07:44:28 +0300

    fefg

--- v1/project_manager/app/orders/index.php
+++ v1/project_manager/app/orders/index.php
@@ -12,174 +12,131 @@ class Orders
 
     function add()
     {
-        $s = getMainRunApp();
-        getManagerLoginToken("RUN_APP", $s);
+        $this->_check("RUN_APP");
         require_once __DIR__ . '/../delivery_men/executer.php';
         return getOrdersExecuter()->executeAddData(getInputUserId(), getInputOrderProductsIdsWithQnt(), 1, getInputUserLocationId(), getInputDeliveryManId());
     }
 
     function addProductToOrder()
     {
-        // $s = getMainRunApp();
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
-        // $projectId = getModelMainRunApp()->app->projectId;
+        $this->_check("RUN_APP");
         return getOrdersProductsExecuter()->executeAddData(getInputOrderId(), getInputProductId(), getInputProductQuantity());
     }
     function addOfferToOrder()
     {
-        // $s = getMainRunApp();
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
-        // $projectId = getModelMainRunApp()->app->projectId;
+        $this->_check("RUN_APP");
         return getOrdersOffersExecuter()->executeAddData(getInputOrderId(), getInputOfferId(), getInputOfferQuantity());
     }
     function read()
     {
-        $s = getMainRunApp();
-        getManagerLoginToken("RUN_APP", $s);
+        $this->_check("RUN_APP");
         return getOrdersExecuter()->executeGetData();
     }
     function readOrderDelivery()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersDeliveryExecuter()->executeGetData(getInputOrderId());
     }
     function updateQuantity()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersProductsExecuter()->executeUpdateQuantity(getInputOrderProductId(), getInputProductQuantity());
     }
     function updateOfferQuantity()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersOffersExecuter()->executeUpdateQuantity(getInputOrderOfferId(), getInputOfferQuantity());
     }
     function updateActualPrice()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersDeliveryExecuter()->executeUpdateActualPrice(getInputOrderDeliveryId(), getInputOrderDeliveryActualPrice());
     }
     function updatePrice()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersDeliveryExecuter()->executeUpdatePrice(getInputOrderDeliveryId(), getInputOrderDeliveryPrice());
     }
     function cencelOrder()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         require_once __DIR__ . '/../orders_cenceled/helper.php';
         return getOrdersProductsExecuter()->executeCencelOrder(getInputOrderId(), getInputOrderCencelDescription());
     }
     function readOrdersOfUsers()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         require_once __DIR__ . '/../users/helper.php';
         return getOrdersProductsExecuter()->executeGetOrdersByUserId(getInputUserId());
     }
     function delete()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersProductsExecuter()->executeDeleteData(getIds());
     }
     function deleteOffers()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersOffersExecuter()->executeDeleteData(getIds());
     }
     function search()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersExecuter()->executeSearchData(getInputOrderId());
     }
     function updateSystemOrderNumber()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersExecuter()->executeUpdateSystemOrder(getInputOrderId(), getInputOrderSystemNumber());
     }
     function readOrderProducts()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersProductsExecuter()->executeGetData(getInputOrderId());
     }
     function readOrderCenceled()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         require_once __DIR__ . '/../orders_cenceled/helper.php';
         return getOrdersCenceledHelper()->getDataByOrderId(getInputOrderId());
     }
     function readUncollectedOrders()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersDeliveryExecuter()->executeGetUncollectedOrders(getInputDeliveryManId());
     }
     function readOrderStatus()
     {
-        // $s = getMainRunApp();
-        // // print_r(getModelMainRunApp()->app->projectId);
-        // $modelUserLoginTokenUserSession = getUserLoginToken("RUN_APP", $s);
-        // $userId = $modelUserLoginTokenUserSession->modelUserSession->userId;
+        $this->_check("RUN_APP");
         return getOrdersStatusExecuter()->executeGetData(getInputOrderId());
     }
 
     ////
     function addDiscount()
     {
+        $this->_check("RUN_APP");
         return getOrdersDiscountsExecuter()->executeAddData(getInputOrderId(), getInputOrderDiscountAmount(), getInputOrderDiscountType());
     }
     function updateType()
     {
+        $this->_check("RUN_APP");
         return getOrdersDiscountsExecuter()->executeUpdateType(getInputOrderDiscountId(), getInputOrderDiscountType());
     }
     function updateAmount()
     {
+        $this->_check("RUN_APP");
         return getOrdersDiscountsExecuter()->executeUpdateAmount(getInputOrderDiscountId(), getInputOrderDiscountAmount());
     }
     function deleteOrderDiscount()
     {
+        $this->_check("RUN_APP");
         return getOrdersDiscountsExecuter()->executeDeleteData(getInputOrderDiscountId());
+    }
+
+    private function _check($permissionName)
+    {
+        $s = getMainRunApp();
+        getManagerLoginToken($permissionName, $s);
     }
 
 
