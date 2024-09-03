f77901e158bce1a84051e3cfc62bbe10cb0c3df5
musta <mustafa774519161@gmail.com>
2024-09-03 13:26:57 +0300

    msjd

--- v1/include/shared_app/order-content/index.php
+++ v1/include/shared_app/order-content/index.php
@@ -2,19 +2,19 @@
 require_once 'helper.php';
 class OrderContent
 {
-    public $orderProducts;
+    public $products;
     public $discount;
     public $delivery;
     public $offers;
 
     function executeGetData($orderId)
     {
-        $orderProducts = (new OrdersProductsHelper())->getDataByOrderId($orderId);
+        $products = (new OrdersProductsHelper())->getDataByOrderId($orderId);
         $delivery = (new OrdersDeliveryHelper())->getDataByOrderId($orderId);
         $discount = (new OrdersDiscountsHelper())->getDataByOrderId($orderId);
         $offers = (new OrdersOffersHelper())->getDataByOrderId($orderId);
         // 
-        $this->orderProducts = $orderProducts;
+        $this->products = $products;
         $this->delivery = $delivery;
         $this->offers = $offers;
         $this->discount = $discount;
