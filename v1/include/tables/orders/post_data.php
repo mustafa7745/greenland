2f27441ef4e52f907f489e6c502c9742e239c5d5
musta <mustafa774519161@gmail.com>
2024-09-12 18:01:21 +0300

    adsd

--- v1/include/tables/orders/post_data.php
+++ v1/include/tables/orders/post_data.php
@@ -12,7 +12,7 @@ function getInputOrderCode()
 function getInputOrderStatusId()
 {
     $name = "inputOrderStatusId";
-    if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
+    if (!isset(getPostData3()[$name])) {
         EMPTY_OR_NOT_FOUND($name);
     }
     $value = getPostData3()[$name];
