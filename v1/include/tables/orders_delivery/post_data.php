7e8c64d2705b49825feb3979815e1abdfd48b1ba
musta <mustafa774519161@gmail.com>
2024-09-14 00:03:31 +0300

    fix empty

--- v1/include/tables/orders_delivery/post_data.php
+++ v1/include/tables/orders_delivery/post_data.php
@@ -33,7 +33,7 @@ function getInputOrderDeliveryId()
 function getInputOrderDeliveryActualPrice()
 {
     $name = "inputOrderDeliveryActualPrice";
-    if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
+    if (!isset(getPostData3()[$name])) {
         EMPTY_OR_NOT_FOUND($name);
     }
     $value = getPostData3()[$name];
