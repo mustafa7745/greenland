4d773d54f64af98ca9a16617ac4b72d7e0f2459b
musta <mustafa774519161@gmail.com>
2024-09-14 23:56:13 +0300

    test remove

--- v1/get_public_key.php
+++ v1/get_public_key.php
@@ -1,14 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin:*");
-$publickey = fread(fopen(__DIR__ . "/include/keys/publicKey.pem", "r"), 10000);
+// header("Access-Control-Allow-Origin:*");
+// $publickey = fread(fopen(__DIR__ . "/include/keys/publicKey.pem", "r"), 10000);
 
-// $key = shared_execute_read1_no_json_sql("SELECT server_key_public FROM server_keys WHERE server_key_id = '1'")[0];
-// sleep(5);
-// die(preg_replace('/^.+\n/','',$publickey));
-$publickey = str_replace('-----BEGIN PUBLIC KEY-----', '', $publickey);
-$publickey = str_replace('-----END PUBLIC KEY-----', '', $publickey);
-$publickey = trim($publickey);
+// // $key = shared_execute_read1_no_json_sql("SELECT server_key_public FROM server_keys WHERE server_key_id = '1'")[0];
+// // sleep(5);
+// // die(preg_replace('/^.+\n/','',$publickey));
+// $publickey = str_replace('-----BEGIN PUBLIC KEY-----', '', $publickey);
+// $publickey = str_replace('-----END PUBLIC KEY-----', '', $publickey);
+// $publickey = trim($publickey);
 
 
-die(json_encode($publickey));
+// die(json_encode($publickey));
+echo "mustafa";
 
