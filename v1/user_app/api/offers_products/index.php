7ae6ea9aaf8e1b09bf9d062121dd6e41ba062ca7
musta <mustafa774519161@gmail.com>
2024-09-17 17:46:30 +0300

    clear 2

--- v1/user_app/api/offers_products/index.php
+++ v1/user_app/api/offers_products/index.php
@@ -21,12 +21,6 @@ class ThisClass
     {
         if (getTag() == "read") {
             return $this->read();
-        } elseif (getTag() == "add") {
-            return $this->add();
-        } elseif (getTag() == "updateQuantity") {
-            return $this->updateQuantity();
-        } elseif (getTag() == "delete") {
-            return $this->delete();
         } else {
             UNKOWN_TAG();
         }
@@ -36,25 +30,8 @@ class ThisClass
     {
         $resultData = $this->controller->read();
         return json_encode($resultData);
-
-    }
-    private function add(): string
-    {
-        $resultData = $this->controller->add();
-        return json_encode($resultData);
     }
 
-    private function updateQuantity(): string
-    {
-        $resultData = $this->controller->updateQuantity();
-        return json_encode($resultData);
-    }
-    // 
-    private function delete(): string
-    {
-        $resultData = $this->controller->delete();
-        return json_encode($resultData);
-    }
 }
 
 $this_class = new ThisClass();
