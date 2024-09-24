383ee832b8fea26332fb21d5f9d2193f732f8fde
musta <mustafa774519161@gmail.com>
2024-09-24 09:59:52 +0300

    fix delete product

--- v1/su/app/products_images/sql.php
+++ v1/su/app/products_images/sql.php
@@ -34,7 +34,7 @@ class ProductsImagesSql extends \ProductsImagesAttribute
         $table_name = $this->table_name;
         $columns = $this->id;
         $innerJoin = "";
-        $condition = "$this->id IN ($ids)";
+        $condition = "$this->productId IN ($ids)";
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
     }
     function addSql($id, $productId, $image): string
