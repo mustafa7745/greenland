1408b7eb8080635947a8bfe4dccc73c389f8d431
musta <mustafa774519161@gmail.com>
2024-09-26 20:37:13 +0300

    wer

--- v1/whatsapp_webhook.php
+++ v1/whatsapp_webhook.php
@@ -47,9 +47,6 @@ if (isset($input)) {
             // $w->sendMessageText("967774519161", "tes");
             $message = mysqli_escape_string(getDB()->conn, $message);
             (new UsersWhatsappUnregisterHelper())->addData($phone_number, $message);
-            (new UsersWhatsappUnregisterHelper())->addData2($phone_number);
-
-            // $w->sendMessageText("967774519161", "tes2");
             exit;
 
         }
