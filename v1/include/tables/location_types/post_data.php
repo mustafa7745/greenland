6ae3c35ffc68887b1994d757b3eefabc203049e6
musta <mustafa774519161@gmail.com>
2024-10-06 22:24:10 +0300

    fix 9

--- v1/include/tables/location_types/post_data.php
+++ v1/include/tables/location_types/post_data.php
@@ -1,16 +1,2 @@
 <?php
 
-function getInputLocationTypeId()
-{
-
-    $name = "inputLocationTypeId";
-    if (!isset(getPostData3()[$name]) || empty(getPostData3()[$name])) {
-        return null;
-    }
-    $value = getPostData3()[$name];
-    if (is_int($value) == false) {
-        return null;
-    }
-    $value = mysqli_real_escape_string(getDB()->conn, $value);
-    return $value;
-}
