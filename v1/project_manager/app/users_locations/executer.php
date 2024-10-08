85fa9839bb736fd0c0332a03cf87a0d4eec86db4
musta <mustafa774519161@gmail.com>
2024-10-08 15:18:44 +0300

    clear

--- v1/project_manager/app/users_locations/executer.php
+++ v1/project_manager/app/users_locations/executer.php
@@ -9,13 +9,7 @@ class UsersLocationsExecuter
     global $PROJECT_ID;
     require_once __DIR__ . '/../../../include/projects/helper.php';
     $data = getUsersLocationsHelper()->getData($userId);
-    // $project = getProjectsHelper()->getDataById($PROJECT_ID);
-    // $project_lat = (getLatLong($project))[0];
-    // $project_long = (getLatLong($project))[1];
     for ($i = 0; $i < count($data); $i++) {
-      // $user_lat = (getLatLong($data[$i]))[0];
-      // $user_long = (getLatLong($data[$i]))[1];
-      // $distanse = haversine_distance($project_lat, $project_long, $user_lat, $user_long);
       $data[$i]['price'] = getDeliveryPrice($data[$i]);;
     }
 
@@ -33,30 +27,12 @@ class UsersLocationsExecuter
      */
     shared_execute_sql("START TRANSACTION");
 
-    // $category_id = uniqid(rand(), false);
-    // require_once __DIR__ . '/../ids_controller/helper.php';
-    // $id = getId(getIdsControllerHelper()->getData($helper->table_name));
     $dataAfterAdd = $helper->addData($userId, $city, $street, $latLong, $nearTo, $contactPhone);
-    // $dataAfterAdd = $helper->getDataById($id);
-
-    // print_r($dataAfterAdd);
-    /**
-     * ADD INSERTED VALUES TO ProjectINSERtOperations TABLE
-     */
+   
 
     shared_execute_sql("COMMIT");
-    // require_once __DIR__ . '/../../../include/projects/helper.php';
-    // global $PROJECT_ID;
-    // $project = getProjectsHelper()->getDataById($PROJECT_ID);
-    // $project_lat = (getLatLong($project))[0];
-    // $project_long = (getLatLong($project))[1];
-    // // 
-    // $user_lat = (getLatLong($dataAfterAdd))[0];
-    // $user_long = (getLatLong($dataAfterAdd))[1];
-    // $distanse = haversine_distance($project_lat, $project_long, $user_lat, $user_long);
 
     $dataAfterAdd['price'] = getDeliveryPrice($dataAfterAdd);
-    //  50 * round(($distanse * getPriceDeliveryPer1Km($project)) / 50);
     return $dataAfterAdd;
   }
   function executeUpdateStreet($id, $newValue)
