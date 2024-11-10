ddb0e4ed0ee2d479298d610e8510733c3e7ba32f
musta <mustafa774519161@gmail.com>
2024-11-10 20:50:38 +0300

    add phone

--- v1/user_app_v1/app/delivery_men/sql.php
+++ v1/user_app_v1/app/delivery_men/sql.php
@@ -9,7 +9,7 @@ class DeliveryMenSql extends \DeliveryMenAttribute
     {
         $table_name = $this->table_name;
         $innerJoin = $this->INNER_JOIN();
-        $columns = "{$this->table_name}.$this->id , {$this->users_attribute->table_name}.{$this->users_attribute->name}";
+        $columns = "{$this->table_name}.$this->id , {$this->users_attribute->table_name}.{$this->users_attribute->name} , {$this->users_attribute->table_name}.{$this->users_attribute->phone}";
         $condition = "{$this->table_name}.$this->id = $id";
         /////
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
