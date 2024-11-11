c505ae2ffce0473c8e22346883ffe29d3f0df0d5
musta <mustafa774519161@gmail.com>
2024-11-11 20:35:08 +0300

    fix 5

--- v1/user_app_v1/app/users_locations/sql.php
+++ v1/user_app_v1/app/users_locations/sql.php
@@ -24,7 +24,7 @@ class UsersLocationsSql extends \UsersLocationsAttribute
         $table_name = $this->table_name;
 
         $innerJoin = $this->INNER_JOIN();
-        $columns = "{$this->table_name}.$this->id, {$this->locations_types_attribute->table_name}.{$this->locations_types_attribute->name} , $this->street , $this->city , $this->latLong , $this->nearTo , $this->url , $this->contactPhone";
+        $columns = "{$this->table_name}.$this->id, {$this->locations_types_attribute->table_name}.{$this->locations_types_attribute->name} as type , $this->street , $this->city , $this->latLong , $this->nearTo , $this->url , $this->contactPhone";
        
 
         // $columns = " * ";
