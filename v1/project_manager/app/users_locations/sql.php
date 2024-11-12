1cd05831f8f2220282242ca7b2e23cec8925808b
musta <mustafa774519161@gmail.com>
2024-11-12 21:14:24 +0300

    fix1

--- v1/project_manager/app/users_locations/sql.php
+++ v1/project_manager/app/users_locations/sql.php
@@ -29,7 +29,7 @@ class UsersLocationsSql extends \UsersLocationsAttribute
         $innerJoin = $this->INNER_JOIN();
         $columns = "{$this->table_name}.$this->id,{$this->table_name}.$this->userId,{$this->table_name}.$this->createdAt,{$this->table_name}.$this->updatedAt, {$this->locations_types_attribute->table_name}.{$this->locations_types_attribute->name} as type , $this->street , $this->city , $this->latLong , $this->nearTo , $this->url , $this->contactPhone";
        
-        $condition = "$this->id = $id FOR UPDATE";
+        $condition = "{$this->table_name}.$this->id = $id FOR UPDATE";
         /////
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
     }
