550bc58b807577da165ed7c0d34a69ee370c2b3a
musta <mustafa774519161@gmail.com>
2024-11-12 21:13:52 +0300

    add type location to pm

--- v1/project_manager/app/users_locations/sql.php
+++ v1/project_manager/app/users_locations/sql.php
@@ -26,8 +26,9 @@ class UsersLocationsSql extends \UsersLocationsAttribute
     function readByIdsql($id): string
     {
         $table_name = $this->table_name;
-        $columns = " * ";
-        $innerJoin = "";
+        $innerJoin = $this->INNER_JOIN();
+        $columns = "{$this->table_name}.$this->id,{$this->table_name}.$this->userId,{$this->table_name}.$this->createdAt,{$this->table_name}.$this->updatedAt, {$this->locations_types_attribute->table_name}.{$this->locations_types_attribute->name} as type , $this->street , $this->city , $this->latLong , $this->nearTo , $this->url , $this->contactPhone";
+       
         $condition = "$this->id = $id FOR UPDATE";
         /////
         return shared_read_sql($table_name, $columns, $innerJoin, $condition);
