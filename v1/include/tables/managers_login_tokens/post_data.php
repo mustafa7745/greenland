af978428bc3cdd8c1303e8bcbb1fcb8003b0045a
musta <mustafa774519161@gmail.com>
2024-11-13 22:08:46 +0300

    fix 2

--- v1/include/tables/managers_login_tokens/post_data.php
+++ v1/include/tables/managers_login_tokens/post_data.php
@@ -4,7 +4,7 @@ function getInputManagerLoginToken()
     // print_r($_POST);
     $name = "inputManagerLoginToken";
     $desc = "M Login Token";
-    // print_r(getPostData2());
+    print_r(getPostData2Encrypted());
     if (!isset(getPostData2Encrypted()[$name])) {
         $ar = "{$desc}_NOT_FOUND";
         $en = "{$desc}_NOT_FOUND";
