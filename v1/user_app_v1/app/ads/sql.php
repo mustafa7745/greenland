59b3c9af99cf7adeec15f974ec58c5ed11c641a3
musta <mustafa774519161@gmail.com>
2024-11-14 17:26:54 +0300

    fix ads

--- v1/user_app_v1/app/ads/sql.php
+++ v1/user_app_v1/app/ads/sql.php
@@ -12,7 +12,8 @@ class AdsSql extends \AdsAttribute
         $table_name = $this->table_name;
         $columns = getColumnImagePath(" * ", "ads_image_path");
         $innerJoin = "";
-        $condition = "DATE($this->createdAt) = DATE('$date') AND $this->isEnabled = $this->ENABLED_STATUS";
-        return shared_read_sql($table_name, $columns, $innerJoin, $condition);
+        $condition = "DATE($this->expireAt) > DATE('$date') AND $this->isEnabled = $this->ENABLED_STATUS";
+        // return shared_read_sql($table_name, $columns, $innerJoin, $condition);
+        return shared_read_order_by_sql($table_name, $columns, $innerJoin, $condition, "`$this->createdAt`", "ASC");
     }
 }
