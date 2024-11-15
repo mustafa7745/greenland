803b51cc9db0d625329df00962a36e3e12e35a70
musta <mustafa774519161@gmail.com>
2024-11-15 15:02:02 +0300

    fix send message

--- v1/include/users_sessions_devices_sessions/sql.php
+++ v1/include/users_sessions_devices_sessions/sql.php
@@ -10,7 +10,7 @@ class UsersSessionsSql extends \UsersSessionsAttribute
         $columns = "{$this->devices_sessions_attribute->appToken}";
 
         $condition = "{$this->userId} = $userId AND {$this->devices_sessions_attribute->appId} = $appId";
-        return shared_read_limit2_sql($table_name, $columns, $innerJoin, "$this->table_name.$this->createdAt", "ASC", $condition, 1);
+        return shared_read_limit2_sql($table_name, $columns, $innerJoin, "$this->table_name.$this->createdAt", "DESC", $condition, 1);
     }
     // function readTokenByUserSessionSql($userSessionId, $appId): string
     // {
